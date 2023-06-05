a7431e513188f5634f706741441db29c82619a95
Hiraishi Taro <tarohiraishi1@gmail.com>
2023-06-05 15:47:09 +0900

    airplay接続状態を確認する

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -41,27 +41,14 @@ class ViewController: UIViewController {
     }
     
     @objc private func handleRouteChange(notification: Notification) {
-        guard
-            let userInfo = notification.userInfo,
-            let reasonValue = userInfo[AVAudioSessionRouteChangeReasonKey] as? UInt,
-            let reason = AVAudioSession.RouteChangeReason(rawValue: reasonValue)
-        else { return }
-        
-        switch reason {
-        case .newDeviceAvailable:
-            let session = AVAudioSession.sharedInstance()
-            for output in session.currentRoute.outputs where output.portType == AVAudioSession.Port.airPlay {
-                print("[debug] AirPlay is connected")
+        print("[debug] route change")
+        let session = AVAudioSession.sharedInstance()
+        for output in session.currentRoute.outputs {
+            if output.portType == .airPlay {
+                print("[debug] connected")
+            } else {
+                print("[debug] disconnected")
             }
-            
-        case .oldDeviceUnavailable:
-            if let previousRoute = userInfo[AVAudioSessionRouteChangePreviousRouteKey] as? AVAudioSessionRouteDescription {
-                for output in previousRoute.outputs where output.portType == AVAudioSession.Port.airPlay {
-                    print("[debug] AirPlay has disconnected")
-                }
-            }
-            
-        default: break
         }
     }
     
